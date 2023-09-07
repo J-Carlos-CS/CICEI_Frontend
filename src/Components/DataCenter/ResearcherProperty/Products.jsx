3ca33ae68af17c2e4bb436748fc2938a63555f92
SebastianLazarte <72448046+SebastianLazarte@users.noreply.github.com>
2023-09-06 20:57:57 -0400

    Changing base

--- src/Components/DataCenter/ResearcherProperty/Products.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Production from '../Production'
-export default function Products({products=null}) {
-  return (
-    <Production products={products}/>
-  )
-}
--- src/Components/Project/ProjectViewForUsersV2/Contexts/ContextProjectUsers.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-const ProjectContext = createContext(null);
-const ActivityContext = createContext(null);
-const ProductContext = createContext(null);
-const UserProjectContext = createContext(null);
-const DiffusionContext = createContext(null);
-export { ProjectContext, ActivityContext, ProductContext, UserProjectContext,DiffusionContext };
