5f643853c0b507e552f362e5634f6dfbb1e0fcd3
SebastianLazarte <72448046+SebastianLazarte@users.noreply.github.com>
2023-09-18 17:51:11 -0400

    Added Equipment CRUD

--- src/scenes/equipment/index.jsx
+++ src/scenes/equipment/index.jsx
@@ -1,16 +1,228 @@
-import React from "react";
-import { Box, useTheme, IconButton } from "@mui/material";
-import { useGetEquipmentQuery } from "state/api";
+import React, { useState } from "react";
+import { Box, useTheme, IconButton,Button,Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  DialogActions,
+  FormControl,
+  InputLabel,
+  Select,MenuItem   } from "@mui/material";
+import {useGetEquipmentQuery,
+  useCreateEquipmentMutation,
+  useUpdateEquipmentMutation,
+  useDeleteEquipmentMutation,
+  useGetProyectsQuery,
+  useGetCategorysQuery  } from "state/api";
 import Header from "components/Header";
 import { DataGrid } from "@mui/x-data-grid";
 import {EditOutlined , 
-  DeleteForeverOutlined } from "@mui/icons-material";
+  DeleteForeverOutlined,
+   } from "@mui/icons-material";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import DataGridCustomToolBar from "components/DataGridCustomToolBar"
 
 const Equipment = () => {
-    const theme = useTheme();
-    const { data, isLoading } = useGetEquipmentQuery();
-  
+  const theme = useTheme();
+  const [createAlertOpen, setCreateAlertOpen] = useState(false);
+  const [updateAlertOpen, setUpdateAlertOpen] = useState(false);
+  const [deleteAlertOpen, setDeleteAlertOpen] = useState(false);
+  const [alertSeverity, setAlertSeverity] = useState('success'); // Puedes cambiar 'success' a 'error' u otro valor según necesites
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const { data, isLoading,refetch } = useGetEquipmentQuery();
+  const { data: categorysData } = useGetCategorysQuery();
+    const { data: proyectsData } = useGetProyectsQuery();
+
+   const [deleteEquipment] = useDeleteEquipmentMutation()
+    const [createEquipment] = useCreateEquipmentMutation();
+    const [updateEquipment] = useUpdateEquipmentMutation();
+
+    const [addEquipmentOpen, setAddEquipmentOpen] = useState(false);
+    const [editEquipmentOpen, setEditEquipmentOpen] = useState(false);
+
+    const [selectedEquipment, setSelectedEquipment] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(""); // Estado para la categoría seleccionada
+    const [selectedProject, setSelectedProject] = useState(""); // Estado para el proyecto seleccionado
     
+  
+    const openAddEquipment = () => {
+      setAddEquipmentOpen(true);
+    };
+  
+    const closeAddEquipment = () => {
+      setAddEquipmentOpen(false);
+    };
+
+    const handleEditEquipment = (selectedEquipment) => {
+      console.log(selectedEquipment)
+      setSelectedEquipment(selectedEquipment);
+      openEditEquipment(selectedEquipment); // Abre el diálogo de edición
+    };
+    const openEditEquipment = (selectedEquipment) => {
+      populateEditForm(selectedEquipment); // Llena el formulario de edición con los datos del Equipo seleccionado
+      setEditEquipmentOpen(true); // Abre el diálogo de edición
+    };
+    const closeEditEquipment = () => {
+      setEditEquipmentOpen(false);
+    };
+    const handleUpdateAlertOpen = () => {
+      setUpdateAlertOpen(true);
+    };
+    const handleUpdateAlertClose = () => {
+      setUpdateAlertOpen(false);
+    };
+    
+    const handleDeleteAlertOpen = () => {
+      setDeleteAlertOpen(true);
+    };
+    const handleDeleteAlertClose = () => {
+      setDeleteAlertOpen(false);
+    };
+    const handleCreateAlertOpen = () => {
+      setCreateAlertOpen(true);
+    };
+    const handleCreateAlertClose = () => {
+      setDeleteAlertOpen(false);
+    };
+    const handleCategoryChange = (event) => {
+      console.log('categaria seleccionado:', event.target.value)
+      setSelectedCategory(event.target.value);
+      setNewEquipment({ ...newEquipment, categoriaId: event.target.value })
+    };
+    const handleCategoryEditChange = (event) => {
+      console.log('categaria seleccionado:', event.target.value)
+      setSelectedCategory(event.target.value);
+      setEditEquipment({ ...editEquipment, categoriaId: event.target.value })
+    };
+    const handleProjectChange = (event) => {
+      console.log('Proyecto seleccionado:', event.target.value)
+      setSelectedProject(event.target.value);
+      setNewEquipment({ ...newEquipment, proyectoId: event.target.value })
+    };
+    const handleProjectEditChange = (event) => {
+      console.log('Proyecto seleccionado:', event.target.value)
+      setSelectedProject(event.target.value);
+      setEditEquipment({ ...editEquipment, proyectoId: event.target.value })
+    };
+      const [newEquipment, setNewEquipment] = useState({
+        nombre: "",
+        cantidad: 0,
+        estado: true,
+        unidad: "",
+      });
+    
+    const saveNewEquipment = async () => {
+      try {
+        const nuevoEquipoData = {
+          nombre: newEquipment.nombre,
+          cantidad: newEquipment.cantidad,
+          estado: newEquipment.estado,
+          unidad: newEquipment.unidad,
+          categoriaId: newEquipment.categoriaId, // Agrega la categoría seleccionada
+          proyectoId: newEquipment.proyectoId,   // Agrega el proyecto seleccionado
+        };
+        
+        // Llama a la función de mutación para crear el nuevo equipo
+        const response = await createEquipment(nuevoEquipoData);
+        
+        if (response.error) {
+          console.error('Error al crear el equipo:', response.error);
+        } else {
+          console.log('Equipo creado con éxito:', response.data);
+          handleCreateAlertOpen();
+            setAlertSeverity('success');
+            setAlertMessage('Equipo creado con éxito.');
+            refetch()
+          refetch();
+        }
+      } catch (error) {
+        console.error('Error al crear el equipo:', error);
+      } finally {
+        // Cierra el modal
+        closeAddEquipment();
+      }
+    };
+    const [editEquipment, setEditEquipment] = useState({
+      id: null,
+      nombre: "",
+      cantidad: "",
+      unidad: "",
+      estado: true,
+      categoriaId: null,
+      proyectoId: null,
+    });
+    const populateEditForm = (selectedEquipment) => {
+      if (selectedEquipment) {
+        setEditEquipment({
+          id: selectedEquipment.id,
+          nombre: selectedEquipment.nombre,
+          cantidad: selectedEquipment.cantidad,
+          unidad: selectedEquipment.unidades,
+          estado: selectedEquipment.estado,
+          categoriaId: selectedEquipment.categoriaId,
+          proyectoId: selectedEquipment.proyectoId,
+        });
+      }
+    };
+    const handleDeleteEquipment = async (id) => {
+      try {
+        // Muestra una confirmación antes de eliminar
+        const confirmed = window.confirm("¿Estás seguro de que deseas eliminar este Equipo?");
+    
+        if (confirmed) {
+          console.log("Equipo A ELIMINAR",id)
+          // Realiza una mutación para cambiar el estado del Equipo a false
+          const response = await deleteEquipment(id);
+    
+          if (response.error) {
+            console.error('Error al eliminar el Equipo:', response.error);
+          } else {
+            console.log('Equipo eliminado con éxito:', response.data);
+            handleDeleteAlertOpen();
+            setAlertSeverity('success');
+            setAlertMessage('Equipo eliminado con éxito.');
+            refetch()
+          
+          }
+        }
+      } catch (error) {
+        console.error('Error al eliminar el Equipo:', error);
+      }
+    };
+    const saveEditedEquipment = async () => {
+      try {
+        
+        const editedEquipmentData = {
+          // Los datos del nuevo Equipo que deseas guardar
+          id: editEquipment.id,
+          nombre: editEquipment.nombre,
+          cantidad: editEquipment.cantidad,
+          unidad: editEquipment.unidades,
+          categoriaId: editEquipment.categoriaId, // Agrega la categoría seleccionada
+          proyectoId: editEquipment.proyectoId,   // Agrega el proyecto seleccionado
+        };
+        // Llama a la función de mutación para crear el nuevo Equipo
+        console.log("id save edited",)
+        const response = await updateEquipment(editedEquipmentData);
+        
+        console.log(editedEquipmentData)
+        if (response.error) {
+          console.error('Error al actualizar el Equipo:', response.error);
+        } else {
+          console.log('Equipo actualizado con éxito:', response.data);
+          handleUpdateAlertOpen();
+          setAlertSeverity('success');
+          setAlertMessage('Equipo actualizado con éxito.');
+          refetch()
+        }
+      } catch (error) {
+        console.error('Error al actualizar el Equipo:', error);
+      } finally {
+        // Cierra el modal
+        closeEditEquipment();
+      }
+    };
     const columns = [
       {
         field: "id",
@@ -33,7 +245,7 @@ const Equipment = () => {
         flex: 0.5,
       },
       {
-        field: "categoria",
+        field: "categoriaId",
         headerName: "Categoría",
         flex: 0.5,
         valueGetter: (params) => params.row.categoria.categoria,
@@ -53,7 +265,7 @@ const Equipment = () => {
         <Box>
             <IconButton
               color="secondary"
-              onClick={console.log("Editar")}
+              onClick={()=>handleEditEquipment(params.row)}
               aria-label="Editar"
             >
               <EditOutlined />
@@ -61,7 +273,7 @@ const Equipment = () => {
   
             <IconButton
               color="secondary"
-              onClick={console.log("Eliminar")}
+              onClick={()=>handleDeleteEquipment(params.row.id)}
               aria-label="Eliminar"
             >
               <DeleteForeverOutlined/>
@@ -72,7 +284,17 @@ const Equipment = () => {
     ];
     return (
       <Box m="1.5rem 2.5rem">
-        <Header title="EQUIPO" subtitle="Lista de las equipos" />
+        <Header title="EQUIPO" subtitle="Lista de los equipos" />
+        <Box display="flex" justifyContent="flex-end" mb="1rem">
+          <Button
+          variant="contained"
+          color="secondary"
+          style={{ fontSize: "1rem", padding: "0.5rem 1rem" }} // Ajustar el tamaño del texto y el espacio interno
+          onClick={openAddEquipment}
+          >
+            Agregar Equipo
+          </Button>
+        </Box>
         <Box
           mt="40px"
           height="75vh"
@@ -106,11 +328,203 @@ const Equipment = () => {
             getRowId={(row) => row.id}
             rows={data || []}
             columns={columns}
+            disableRowSelectionOnClick
+            components={{Toolbar: DataGridCustomToolBar}}
           />
         </Box>
-      </Box>
-    );
-  };
-  
+        
+      {/*Dialog Agregar Equipo*/ }  
+      <Dialog open={addEquipmentOpen} onClose={closeAddEquipment}>
+        <DialogTitle>Agregar Nuevo Equipo</DialogTitle>
+          <DialogContent>
+          <form>
+          <TextField
+          label="Nombre"
+          value={newEquipment.nombre}
+          onChange={(e) =>
+            setNewEquipment({ ...newEquipment, nombre: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Cantidad"
+          type="number"
+          value={newEquipment.cantidad}
+          onChange={(e) =>
+            setNewEquipment({ ...newEquipment, cantidad: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Unidad"
+          value={newEquipment.unidad}
+          onChange={(e) =>
+            setNewEquipment({ ...newEquipment, unidad: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        <Box display= "flex" justifyContent="space-between" mt="1rem" mb="1rem">
+          {/* Dropdown para Categoría */}
+          <FormControl >
+            <InputLabel htmlFor="category-select">Categoría</InputLabel>
+            
+            <Select
+            
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+              inputProps={{
+                name: 'category',
+                id: 'category-select',
+              }}
+              style={{ minWidth: '200px' }} // Aumenta el ancho de la caja
+            >
+              {categorysData && categorysData.map((category) => (
+                <MenuItem key={category.id} value={category.id}>
+                  {category.categoria}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          {/* Dropdown para Proyecto */}
+            <FormControl>
+              <InputLabel htmlFor="proyect-select">Proyecto</InputLabel>
+              <Select
+                value={selectedProject}
+                onChange={handleProjectChange}
+                inputProps={{
+                  name: 'proyect',
+                  id: 'proyect-select',
+                }}
+                style={{ minWidth: '200px' }} // Aumenta el ancho de la caja
+              >
+                {proyectsData && proyectsData.map((proyect) => (
+                  <MenuItem key={proyect.id} value={proyect.id}>
+                    {proyect.proyecto}
+                  </MenuItem>
+                ))}
+              </Select>
+          </FormControl>
+        </Box>
+        </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={closeAddEquipment} color="secondary">
+              Cancelar
+            </Button>
+            <Button onClick={saveNewEquipment} color="secondary">
+              Guardar
+            </Button>
+          </DialogActions>
+      </Dialog>
+       {/*Dialog Editar Equipo*/ }  
+       <Dialog open={editEquipmentOpen} onClose={closeEditEquipment}>
+        <DialogTitle>Editar Equipo</DialogTitle>
+          <DialogContent>
+          <form>
+          <TextField
+          label="Nombre"
+          value={editEquipment.nombre}
+          onChange={(e) =>
+            setEditEquipment({ ...editEquipment, nombre: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Cantidad"
+          type="number"
+          value={editEquipment.cantidad}
+          onChange={(e) =>
+            setEditEquipment({ ...editEquipment, cantidad: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Unidad"
+          value={editEquipment.unidad}
+          onChange={(e) =>
+            setEditEquipment({ ...editEquipment, unidad: e.target.value })
+          }
+          fullWidth
+          margin="normal"
+        />
+        
+        
+        <Box display= "flex" justifyContent="space-between" mt="1rem" mb="1rem">
+          {/* Dropdown para Categoría */}
+          <FormControl >
+            <InputLabel htmlFor="category-select">Categoría</InputLabel>
+            
+            <Select
+            
+              value={editEquipment.categoriaId}
+              onChange={handleCategoryEditChange}
+              inputProps={{
+                name: 'category',
+                id: 'category-select',
+              }}
+              style={{ minWidth: '200px' }} // Aumenta el ancho de la caja
+            >
+              {categorysData && categorysData.map((category) => (
+                <MenuItem key={category.id} value={category.id}>
+                  {category.categoria}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          {/* Dropdown para Proyecto */}
+            <FormControl>
+              <InputLabel htmlFor="proyect-select">Proyecto</InputLabel>
+              <Select
+                value={editEquipment.proyectoId}
+                onChange={handleProjectEditChange}
+                inputProps={{
+                  name: 'proyect',
+                  id: 'proyect-select',
+                }}
+                style={{ minWidth: '200px' }} // Aumenta el ancho de la caja
+              >
+                {proyectsData && proyectsData.map((proyect) => (
+                  <MenuItem key={proyect.id} value={proyect.id}>
+                    {proyect.proyecto}
+                  </MenuItem>
+                ))}
+              </Select>
+          </FormControl>
+        </Box>
+
+        </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={closeEditEquipment} color="secondary">
+              Cancelar
+            </Button>
+            
+            <Button onClick={saveEditedEquipment} color="secondary">
+              Guardar
+    
+            </Button>
+            
+          </DialogActions>
+          </Dialog>
+      <Snackbar
+                open={updateAlertOpen || deleteAlertOpen|| createAlertOpen}
+                autoHideDuration={4000} // Controla cuánto tiempo se muestra la alerta (en milisegundos)
+                onClose={handleUpdateAlertClose} // Puedes usar handleDeleteAlertClose para la alerta de eliminación
+                >
+              <Alert severity={alertSeverity} onClose={handleUpdateAlertClose}>
+                {alertMessage}
+                </Alert>
+            </Snackbar> 
+    </Box>
+    
+  );
+};
 
 export default Equipment
--- src/state/api.js
+++ src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/laboratorio/api/"} ) ,
     reducerPath: "adminApi" ,
-    tagTypes: ["User", "Proyects", "Categorys","Reactives"],
+    tagTypes: ["User", "Proyects", "Categorys","Reactives","Equipment"],
     endpoints: (build) => ( {
         getUser: build.query({
             query: (id) => `user/${id}`,
@@ -57,7 +57,35 @@ export const api = createApi({
         getEquipment: build.query({
             query: () => `equipos`,
             providesTags: ["Equipos"]
-        })
+        }),
+        createEquipment: build.mutation({
+            query: (newEquipment) => ({
+              url: 'equipos', // La URL para agregar reactivos
+              method: 'POST', // Método POST para agregar
+              body: newEquipment, // Datos del nuevo reactivo
+            }),
+            // Define cómo se etiquetará la caché después de agregar un equipo
+            invalidatesTags: ['Equipment'],
+        }),
+        updateEquipment: build.mutation({
+            query: (equipmentData) => ({
+              url: `equipos/${equipmentData.id}`, // La URL para agregar Equipos
+              method: 'PUT', // Método PUT para editar
+              body: equipmentData, // Datos del Equipo
+            }),
+          
+            invalidatesTags: ['Equipment'],
+        }),
+        deleteEquipment: build.mutation({
+            
+            query: (id) => ({
+              url: `equipos/${id}`, // La URL para eliminar reactivo
+              method: 'DELETE', // Método DELETE para eliminar logicamente
+             
+            }),
+            
+            invalidatesTags: ['Equipment'],
+        }),
         
     }
    
@@ -74,4 +102,7 @@ export const {useGetUserQuery,
     useUpdateReactiveMutation,
     /* Equipment*/
     useGetEquipmentQuery,
+    useCreateEquipmentMutation,
+    useUpdateEquipmentMutation,
+    useDeleteEquipmentMutation
     }= api
